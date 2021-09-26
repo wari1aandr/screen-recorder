4442eb90eca4bfc1729e56867c3934d2e8da9504
wari1aandr <qalska7@gmail.com>
2021-09-26 23:24:55 +0300

    верстка

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
-  <div id="app">
+  <div id="app"
+  class="recorder">
+
+    <div>
+      <h1 class="recorder__h1">screen recorder</h1>
+    
+      <div class="recorder__buttons">
+
+        <button class="recorder__btn"
+          v-if="!isRecording"
+          @click="handleBtn()">
+          Start recorder
+        </button>
+        <button class="recorder__btn"
+          v-if="isRecording"
+          @click="handleBtn()">
+          Stop
+        </button>
+
+      </div>
+    </div>
 
   </div>
 </template>
@@ -8,11 +28,59 @@
 
 export default {
   name: 'App',
-  components: {
+  data() {
+    return {
+      isRecording: false,
+    }
+  },
+  methods: {
+    handleBtn() {
+      this.isRecording = !this.isRecording;
+    }
   }
 }
 </script>
 
 <style lang="scss">
+body {
+  background: #1b2631;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.recorder {
+  display: flex;
+  justify-content: center;
 
+  &__h1 {
+    font-size: 45px;
+    letter-spacing: 3px;
+    color: #f1c40f;
+  }
+
+  &__buttons {
+    display: flex;
+    justify-content: center;
+  }
+
+  &__btn {
+    background: #1b2631;
+    display: inline-block;
+    margin: 10px 20px;
+    padding: 10px 30px;
+    position: relative;
+    border: 2px solid #f1c40f;
+    color: #f1c40f;
+    font-family: Arial, Helvetica, sans-serif;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: 600;
+    cursor: pointer;
+    transition: 0.8s;
+
+    &:hover {
+      background: #f1c40f;
+      color: #1b2631;
+    }
+
+  }
+}
 </style>
