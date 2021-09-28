d66ca545ff9fadb237f2928c481f3b40738d222b
wari1aandr <qalska7@gmail.com>
2021-09-28 21:47:31 +0300

    дополнила верстку, options, start и stoprecording

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,38 @@
   <div id="app"
   class="recorder">
 
-    <div>
+    <div class="recorder__header">
       <h1 class="recorder__h1">screen recorder</h1>
     
       <div class="recorder__buttons">
 
         <button class="recorder__btn"
           v-if="!isRecording"
-          @click="handleBtn()">
+          @click="startRecording()">
           Start recorder
         </button>
+
         <button class="recorder__btn"
           v-if="isRecording"
-          @click="handleBtn()">
+          @click="stopRecording()">
           Stop
+        </button>
+
+      </div>
+    </div>
+
+
+    <div class="recorder__wrapper">
+      <div class="recorder__footer" v-if="isRecording">
+
+        <video 
+        class="recorder__video"
+        src=""
+        controls="controls"></video>
+
+        <button class="recorder__btn"
+          @click="downloadVideo()">
+          Download
         </button>
 
       </div>
@@ -31,11 +49,42 @@ export default {
   data() {
     return {
       isRecording: false,
+      options: { mimeType: 'video/webm' },
+      displayMediaOptions: {
+        video: {
+          cursor: "always",
+          logicalSurface: false
+        },
+        audio: {
+          echoCancellation: false,
+          noiseSuppression: false,
+          sampleRate: 44100
+        }
+      },
+      recordedChunks: [],
+      stream: {},
+      mediaRecorder: {}
     }
   },
   methods: {
-    handleBtn() {
-      this.isRecording = !this.isRecording;
+    async startRecording() {
+      this.isRecording = true;
+      
+      this.stream = await navigator.mediaDevices.getDisplayMedia(this.displayMediaOptions);
+      this.mediaRecorder = new MediaRecorder(this.stream, this.options);
+      this.mediaRecorder.ondataavailable = this.handleDataAvailable;
+      this.mediaRecorder.start();
+
+    },
+    handleDataAvailable(event) {
+      if (event.data.size > 0) {
+        this.recordedChunks.push(event.data);
+      }
+    },
+    stopRecording() {
+      this.isRecording = false;
+      
+      this.mediaRecorder.stop();
     }
   }
 }
@@ -47,13 +96,11 @@ body {
   font-family: Arial, Helvetica, sans-serif;
 }
 .recorder {
-  display: flex;
-  justify-content: center;
-
   &__h1 {
     font-size: 45px;
     letter-spacing: 3px;
     color: #f1c40f;
+    text-align: center;
   }
 
   &__buttons {
@@ -62,11 +109,10 @@ body {
   }
 
   &__btn {
+    display: block;
     background: #1b2631;
-    display: inline-block;
-    margin: 10px 20px;
+    margin: 10px auto;
     padding: 10px 30px;
-    position: relative;
     border: 2px solid #f1c40f;
     color: #f1c40f;
     font-family: Arial, Helvetica, sans-serif;
@@ -80,7 +126,12 @@ body {
       background: #f1c40f;
       color: #1b2631;
     }
+  }
 
+  &__video {
+    display: block;
+    width: 50%;
+    margin: 30px auto;
   }
 }
 </style>
